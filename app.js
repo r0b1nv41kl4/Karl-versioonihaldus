55aa77f87323c27cba4cb99de0ef53017fe956e6
r0b1nv41kl4 <robin.vaikla@ametikool.ee>
2026-01-28 18:10:59 +0200

    updated app.js, index.html and style.css

--- app.js
+++ app.js
@@ -1,51 +1,54 @@
-const scoreE1 = document.getElementById('score')
-const comboE1 = document.getElementById('combo')
-const clickBtn = document.getElementById('clickBtn')
-const resetBtn = document.getElementById('resetBtn')
-const powerBtn = document.getElementById('powerBtn')
-const toastE1 = document.getElementById('toast')
+const scoreEl = document.getElementById("score");
+const comboEl = document.getElementById("combo");
+const clickBtn = document.getElementById("clickBtn");
+const resetBtn = document.getElementById("resetBtn");
+const powerBtn = document.getElementById("powerBtn");
+const toastEl = document.getElementById("toast")
 
 let score = 0;
 let combo = 1;
 let lastClickAt = 0;
 let powerActive = false;
 let powerTimeoutId = null;
-let powerPerClick = 1;
+let pointsPerClick = 1;
+
+const btnclasses = ["btn-a", "btn-b", "btn-c", "btn-d", "btn-e"];
+let btnClassIndex = 0;
 
 function render() {
-  scoreE1.textContent = String(score);
-  comboE1.textContent = "x" + String(combo);
+    scoreEl.textContent = String(score);
+    comboEl.textContent = "x" + String(combo);
 }
 
 function updateCombo(now) {
-  const dt = now -lastClickAt;
-  if (dt > 0 && dt < 450) combo = Math.min(5,combo +1);
-  else combo = 1
+    const dt = now -lastClickAt;
+    if ( dt > 0 && dt < 450) combo = Math.min(5,combo +1 );
+    else combo = 1;
 }
 
-function cycleButtonColor() {
-  clickBtn.classList.remove(btnclasses[btnClassIndex]);
-  btnClassIndex = (btnClassIndex + 1) %
-  btnclasses.length;
-  clickBtn.classList.add(btnclasses[btnClassIndex]);
+function cycleButtoncolor() {
+    clickBtn.classList.remove(btnclasses[btnClassIndex]);
+    btnClassIndex = (btnClassIndex + 1) %
+    btnclasses.length;
+    clickBtn.classList.add(btnclasses[btnClassIndex]);
 }
 
 function toast(msg) {
-  toastE1.hidden = false;
-  toastE1.textContent = msg;
-  setTimeout(()=> toastE1.hidden = true); 1200;
+    toastEl.hidden = false;
+    toastEl.textContent = msg;
+    setTimeout(() => {toastEl.hidden = true; }, 1200);
 }
 
-clickBtn.addEventListener('click', () => {
-  const now = Date.now();
-  updateCombo(now);
+clickBtn.addEventListener("click", () => {
+    const now = Date.now();
+    updateCombo(now);
 
-  const multiplayer = combo * (powerActive ? 2 : 1);
-  score += (pointsPerClick * multiplayer);
+    const multiplayer = combo * (powerActive ? 2 : 1);
+    score += (pointsPerClick * multiplayer);
 
-  cycleButtonColor();
-  lastClickAt = now;
+    cycleButtoncolor();
+    lastClickAt = now;
 
-  if (combo === 5) toast('max combo miees');
-  render();
-})
+    if ( combo === 5) toast("MAX COMBO!");
+    render();
+});
